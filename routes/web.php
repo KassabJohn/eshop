32c0f81fa1e7e370a713148310d91235464f435f
John <john.kassab@ynov.com>
2021-01-12 21:00:20 +0100

    ajout routes into web.php

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,26 @@ Route::get('/signup', 'signupController@userIndex')->name('User.signup');
 Route::post('/signup', 'signupController@userPosted');
 Route::post('/check_email', 'signupController@emailCheck')->name('User.signup.check_email');
 Route::get('/logout', 'loginController@userLogout')->name('User.logout');
-Route::get('/history', 'User\userController@history')->name('User.history');
+Route::get('/', 'user\userController@index')->name('user.home');
+Route::get('/product/{id}', 'user\userController@view')->name('user.product');
+
+Route::get('/search', 'user\userController@search')->name('user.search');
+Route::get('/search?c={id}', 'user\userController@view')->name('user.search.cat');
+
+
+
+Route::get('/view/{id}', 'user\userController@view')->name('user.view');
+Route::post('/view/{id}', 'user\userController@post');
+
+Route::get('/cart', 'user\userController@cart')->name('user.cart');
+Route::post('/cart', 'user\userController@confirm');
+
+Route::post('/edit_cart', 'user\userController@editCart')->name('user.editCart');
+Route::post('/delete_item_from_cart', 'user\userController@deleteCartItem')->name('user.deleteCartItem');
+
+
+Route::get('/logout', 'loginController@userLogout')->name('user.logout');
+
+Route::group(['middleware' => 'user'], function(){
+    Route::get('/history', 'user\userController@history')->name('user.history');
+});
