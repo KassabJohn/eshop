e890ad3379ab865b60fdf1815f8220fa32d24a82
John <john.kassab@ynov.com>
2021-01-15 12:03:30 +0100

    modif productphp + style

--- resources/views/store/product.blade.php
+++ resources/views/store/product.blade.php
@@ -1,49 +1,49 @@
 @extends('store.storeLayout')
 @section('content')
 
+    <style>
+        .card-horizontal {
+            display: flex;
+            flex: 1 1 auto;
+        }
+        img {
+            width: 100%;
+            height: auto;
+        }
+    </style>
 
-<div class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5 ">
-                <div id="product-main-img">
-                    <div class="product-preview">
-                        <img src="../uploads/products/{{$product->id}}/{{$product->image_name}}" alt="">
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-5">
-                <div class="product-details">
-                    <h2 class="product-name">{{$product->name}}</h2>
-                    <div>
-                        <h3 class="product-price">{{$product->discount}} €</h3><h6><del class="product-old-price"><br>{{$product->price}} €</del></h6>
-                    </div>
-                    <p>{{$product->description}}</p>
+    <br>
+    <br>
+    <br>
+    <div class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 ">
                     <form method="post" id="order_form">
-                    @csrf
-                    <div class="product-options" >
-                        <input type="hidden" id="discount_price_holder" name="discount_price_holder" value={{$product->discount}}>
-                        <input type="number" id="quantity" name="quantity" value="1" min="1" max="100" />
-                    </div>
-                        @foreach($colors as $c)
-                            <input type="radio" name="color"  value="{{$c}}">
-                            <div style="height:25px;width:25px;margin:5px;display:inline-block;background-color: {{$c}}"></div>
-                        @endforeach
-                    <div class="add-to-cart">
-                        <br>
-                        <button type="submit" name="myButton" id="myButton" class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
-                    </div>
-                    </form>
-                    <ul class="product-links">
-                        <ul>Category:</ul>
-                        <li><a href="{{route('user.search')}}?c={{$product->category->id}}">{{$product->category->name}}</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div style="height:200px"></div>
+                        @csrf
+                        <div class="card">
+                            <div class="card-horizontal">
+                                <div class="img-square-wrapper">
+                                    <img class="" src="../uploads/products/{{$product->id}}/{{$product->image_name}}" alt="">
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title">{{$product->name}}</h4>
+                                    <p class="card-text"><h3>{{$product->discount}} €</h3><h6><del class="product-old-price"><br>{{$product->price}} €</del></h6>
+                                </div>
 
+                            </div>
+                            <div class="card-footer">
+
+                                @foreach($colors as $c)
+                                    <input type="radio" name="color"  value="{{$c}}">
+                                    <div style="height:25px;width:25px;margin:5px;display:inline-block;background-color: {{$c}}"></div>
+                                    @endforeach
+                                    </small>
+                                    <br>
+                                    <button type="submit" name="myButton" id="myButton" class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> add to cart</button>
+                                    <small class="text-muted">
+                                        <input type="number" id="quantity" name="quantity" value="1" min="1" max="100" />
+                            </div>
+                        </div>
+                    </form>
 @endsection
