6d26c70d3694f0b303be4630d67c721fc464c271
John <john.kassab@ynov.com>
2021-01-15 11:25:59 +0100

    ajout search.php + recherche par categ

---
+++ resources/views/store/search.blade.php
@@ -0,0 +1,33 @@
+@extends('store.storeLayout')
+@section('content')
+<div class="section">
+    <div class="container">
+        <div class="row">
+            <div id="store" class="col-md-12">
+                <div class="col-md-12">
+                    <div class="row">
+                    @foreach($products as $product)
+                            <div class="col-3">
+                                <br>
+                                <br>
+                                <div class="card">
+                                    <img src="uploads/products/{{$product->id}}/{{$product->image_name}}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h6 class="card-title"><a href="{{route('user.view',['id'=>$product->id])}}">{{$product->name}}</a></h6>
+                                    </div>
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">{{$product->discount}} €</li>
+                                        <del><li class="list-group-item">{{$product->price}} €</li></del>
+                                    </ul>
+                                    <div class="card-body">
+                                        <a href="{{route('user.view',['id'=>$product->id])}}" class="card-link">Buy The Game</a>
+                                    </div>
+                                </div>
+                            </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endsection
