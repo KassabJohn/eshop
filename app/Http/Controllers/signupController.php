4135ee49bafd771b47f82799596a791b6987de94
John <john.kassab@ynov.com>
2021-01-17 15:27:08 +0100

    Ajout Controller Sign in + routes

---
+++ app/Http/Controllers/signupController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use App\Category;
+use App\User;
+use App\Address;
+
+
+class signupController extends Controller
+{
+    public function userIndex()
+    {
+
+        if (session()->has('user')) {
+            return redirect()->route("user.cart");
+        }
+
+        $res = Product::all();
+        $cat = Category::all();
+
+
+        return view('store.signup')
+            ->with('products', $res)
+            ->with("cat", $cat);
+    }
+
+    public function userPosted(Request $r)
+    {
+        $u = new User();
+        $add = new Address();
+        $add->area = $r->address;
+        $add->city = $r->city;
+        $add->zip = $r->zip;
+
+        $add->save();
+        $add_id = $add->id;
+
+        $u->full_name = $r->name;
+        $u->email = $r->email;
+        $u->password = $r->pass;
+        $u->address_id = $add_id;
+        $u->phone = $r->tel;
+
+        $u->save();
+
+        $user = User::find($u->id);
+
+        $r->session()->put('user', $user);
+
+        return redirect()->route('user.home');
+    }
+}
+
