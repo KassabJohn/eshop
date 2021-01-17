9ef840a0fe11822ae5c7851351bea61b7f282287
John <john.kassab@ynov.com>
2021-01-17 15:20:05 +0100

    Ajout Sign In form

--- resources/views/store/signup.blade.php
+++ resources/views/store/signup.blade.php
@@ -1,52 +1,58 @@
 @extends('store.storeLayout')
 @section('content')
 
+    <div class="row">
+        <div class="row justify-content-md-center">
+            <div class="col-sm-4">
+                <br>
 
-<div class="section">
-    <div class="container">
-        <div class="row">
-
-        <form id="signupForm" method="post" >
-            @csrf
-            <div class="col-md-12">
-                <div class="billing-details">
-                    <div class="section-title">
-                        <h3 class="title">SIGN UP</h3>
-                    </div>
-
-                    <div class="form-group ">
-                        <input class="input" type="text" name="name" id="name" placeholder="Full Name">
+                <form id="signupForm" method="post" >
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Full Name</label>
+                        <input class="form-control" type="text" name="name" id="name" placeholder="Full Name">
                     </div>
 
                     <div class="form-group">
-                        <input class="input" type="email" name="email" id="email" placeholder="Email">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email">
                     </div>
-                    <div id="for_duplicate-email"></div>
+
                     <div class="form-group">
-                        <input class="input" type="text" name="address" id="address" placeholder="Address">
+                        <label for="exampleInputEmail1">Address</label>
+                        <input class="form-control" type="text" name="address" id="address" placeholder="Address">
                     </div>
+
                     <div class="form-group">
-                        <input class="input" type="text" name="city" id="city" placeholder="City">
+                        <label for="exampleInputEmail1">City</label>
+                        <input class="form-control" type="text" name="city" id="city" placeholder="City">
                     </div>
+
                     <div class="form-group">
-                        <input class="input" type="number" name="zip" id="zip" placeholder="ZIP Code">
+                        <label for="exampleInputEmail1">ZIP Code</label>
+                        <input class="form-control" type="number" name="zip" id="zip" placeholder="ZIP Code">
                     </div>
+
                     <div class="form-group">
-                        <input class="input" type="number" name="tel" id="tel" placeholder="Telephone">
+                        <label for="exampleInputEmail1">Telephone</label>
+                        <input class="form-control" type="number" name="tel" id="tel" placeholder="Telephone">
                     </div>
+
                     <div class="form-group">
-                        <input class="input" type="password" name="pass" id="pass" placeholder="Enter Your Password">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" name="pass" id="pass" placeholder="Password">
                     </div>
+
                     <div class="form-group">
-                        <input class="input" type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
+                        <label for="exampleInputPassword1">Confirm Password</label>
+                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
                     </div>
-                    <br>
-                        <input type="submit"  name="signup" class="primary-btn order-submit" value="Sign Up">
-                </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+
             </div>
-        </form>
         </div>
     </div>
-</div>
 @endsection
 
