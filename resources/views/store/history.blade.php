cf4c51a9eb334a8497e4582d1bc698bc863c8744
John <john.kassab@ynov.com>
2021-01-18 14:42:54 +0100

    ajout html css page history paiement

---
+++ resources/views/store/history.blade.php
@@ -0,0 +1,42 @@
+@extends('store.storeLayout')
+@section('content')
+
+        <div class="row">
+            <div class="row justify-content-md-center">
+            <div class="col-md-10">
+                <table class="table table-striped">
+                    <thead>
+                        <th>Order Id</th>
+                        <th>Image </th>
+                        <th>Name</th>
+                        <th>Color</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody>
+                        @foreach($sale as $s)
+                            @foreach($all as $c)
+                            @if($c[0]==$s->id)
+                            @foreach($products as $p)
+                            @if(session('user')->id == $s->user_id)
+                                @if($c[1]==$p->id)
+                                <tr>
+                                <td>{{$s->id}}</td>
+                                <td><img src="uploads/products/{{$p->id}}/{{$p->image_name}}" height="10%" width="10%"></td>
+                                <td>{{$p->name}}</td>
+                                <td><div style="height:25px;width:25px;margin:5px;display:inline-block;background-color: {{$c[3]}}"></div></td>
+                                <td>{{$s->order_status}}</td>
+                                </tr>
+                                @break
+                                @endif
+                                @endif
+                            @endforeach
+                        @endif
+                        @endforeach
+                        @endforeach
+                        </tbody>
+                </table>
+            </div>
+        </div>
+        </div>
+
+@endsection
