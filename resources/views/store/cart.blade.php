fdd3b69b8006d46ac94dc4762488ae73c8236186
John <john.kassab@ynov.com>
2021-01-20 10:21:25 +0100

    Ajout page paiement + validation paiement

---
+++ resources/views/store/cart.blade.php
@@ -0,0 +1,53 @@
+@extends('store.storeLayout')
+@section('content')
+
+    @if($all != null)
+        <div class="row">
+            <div class="row justify-content-md-center">
+                <div class="col-sm-10">
+                    <table class="table table-striped">
+                        <thead>
+                        <th scope="col"><strong>PRODUCT NAME</strong></th>
+                        <th scope="col"><strong>IMAGE</strong></th>
+                        <th scope="col"><strong>QUANTITY</strong></th>
+                        </thead>
+                        @foreach($all as $c)
+                            @foreach($prod as $p)
+                                @if($c[0]==$p->id)
+                                    <tbody>
+                                    <tr>
+                                        <td>{{$p->name}}</td>
+                                        <td><img src="uploads/products/{{$p->id}}/{{$p->image_name}}" height="50px" width="50px"> {{$p->name}}</td>
+                                        <td><input type="number"  id="quantity" style="width:15%" name={{$p->id}} value={{$c[1]}} min="1" max="100" readonly/></td>
+                                    @break
+                                    @endif
+                                    @endforeach
+                                    @endforeach
+                                    </tbody>
+                    </table>
+                    <div class="card text-center">
+                        <div class="card-header">
+                            Cart Validation
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Total :</h5>
+                            <p class="card-text"><div><strong id="totalCost">{{Session::get('price')}} €</strong></div>
+                            @if(session('user'))
+                                @if($all != null)
+                                    <center> <form method="post" name="cart">
+                                            @csrf
+                                            <input type="submit" id="confirm_order"  name="order" value="Confirm order">
+                                        </form></center>
+                                @endif
+                            @endif
+                        </div>
+                    </div>
+                    @else
+                        <div class="jumbotron jumbotron-fluid">
+                            <div class="container">
+                                <h1 class="display-4">Your Cart is Empty</h1>
+                                <p class="lead">You need to add something to your cart</p>
+                            </div>
+                        </div>
+    @endif
+@endsection
