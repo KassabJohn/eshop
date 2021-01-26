78fba0ed45f863e83446cddae9ffd15e3240bee9
John <john.kassab@ynov.com>
2021-01-26 01:11:41 +0100

    css ordermanagment

--- resources/views/admin_panel/dashboard/index.blade.php
+++ resources/views/admin_panel/dashboard/index.blade.php
@@ -1,33 +1,39 @@
 @extends('admin_panel.adminLayout') @section('content')
-                        <h4>Orders</h4>
-                            <table>
-                                <thead>
-                                <tr>
-                                    <th>
-                                        Time
-                                    </th>
-                                    <th>
-                                        Status
-                                    </th>
-                                    <th>
-                                        Amount
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($sales as $s)
-                                    <tr>
-                                        <td class="font-weight-medium">
-                                            {{$s->created_at}}
-                                        </td>
-                                        <td class="font-weight-medium">
-                                            {{$s->order_status}}
-                                        </td>
-                                        <td class="font-weight-medium">
-                                            {{$s->price}}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+
+                                        <center>
+       <b><h4>Orders</h4></b>
+        <div class="col-7">
+        <table class="table table-striped">
+            <thead class="thead-dark">
+            <tr>
+                <th>
+                    Time
+                </th>
+                <th>
+                    Status
+                </th>
+                <th>
+                    Amount
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($sales as $s)
+                <tr>
+                    <td class="font-weight-medium">
+                        {{$s->created_at}}
+                    </td>
+                    <td class="font-weight-medium">
+                        {{$s->order_status}}
+                    </td>
+                    <td class="font-weight-medium">
+                        {{$s->price}}
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        </div>
+    </div>
+                                        </center>
 @endsection
