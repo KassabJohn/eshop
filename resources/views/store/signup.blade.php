5fff3b2946a45000066cb79de2273aad4a4f6f3e
John <john.kassab@ynov.com>
2021-01-26 08:58:32 +0100

    required formulaire log sin

--- resources/views/store/signup.blade.php
+++ resources/views/store/signup.blade.php
@@ -9,42 +9,42 @@
                     @csrf
                     <div class="form-group">
                         <label for="exampleInputEmail1">Full Name</label>
-                        <input class="form-control" type="text" name="name" id="name" placeholder="Full Name">
+                        <input class="form-control" type="text" name="name" id="name" placeholder="Full Name" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email</label>
-                        <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Address</label>
-                        <input class="form-control" type="text" name="address" id="address" placeholder="Address">
+                        <input class="form-control" type="text" name="address" id="address" placeholder="Address" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">City</label>
-                        <input class="form-control" type="text" name="city" id="city" placeholder="City">
+                        <input class="form-control" type="text" name="city" id="city" placeholder="City" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">ZIP Code</label>
-                        <input class="form-control" type="number" name="zip" id="zip" placeholder="ZIP Code">
+                        <input class="form-control" type="number" name="zip" id="zip" placeholder="ZIP Code" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Telephone</label>
-                        <input class="form-control" type="number" name="tel" id="tel" placeholder="Telephone">
+                        <input class="form-control" type="number" name="tel" id="tel" placeholder="Telephone" required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" name="pass" id="pass" placeholder="Password">
+                        <input type="password" class="form-control" name="pass" id="pass" placeholder="Password"required>
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputPassword1">Confirm Password</label>
-                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Password">
+                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
